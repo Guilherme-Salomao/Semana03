ed8809341de650aa07559f2c21ef00ef61755c8b
Guilherme-Salomao <64329822+Guilherme-Salomao@users.noreply.github.com>
2024-02-07 20:24:11 -0300

    Atividade 5

---
+++ src/Atividade5.java
@@ -0,0 +1,16 @@
+public class Atividade5 {
+    public static void main(String[] args) {
+        String livro0 = "Livro";
+        double preco0 = 49.95;
+        String livro1 = "Celular";
+        double preco1 = 140.76;
+
+        System.out.println(">>> Listagem dos Produtos");
+        System.out.println("_________________________");
+        System.out.println("Nome: " + livro0);
+        System.out.println("Preço: R$ " + preco0);
+        System.out.println("_________________________");
+        System.out.println("Nome: " + livro1);
+        System.out.println("Preço: R$ " + preco1);
+    }
+}
