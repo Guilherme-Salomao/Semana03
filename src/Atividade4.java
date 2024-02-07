60c64b01aee3ee093d5b055a8eff57a82c496a96
Guilherme-Salomao <64329822+Guilherme-Salomao@users.noreply.github.com>
2024-02-07 20:19:52 -0300

    Atividade 4

---
+++ src/Atividade4.java
@@ -0,0 +1,17 @@
+public class Atividade4 {
+    public static void main(String[] args) {
+
+        String nome0 = "Fulano";
+        int data0 = 1980;
+        String nome1 = "Sicrano";
+        int data1 = 2000;
+
+        System.out.println(">>> Listagem dos Clientes");
+        System.out.println("_________________________");
+        System.out.println("Nome: " + nome0);
+        System.out.println("Data de Nascimento: " + data0);
+        System.out.println("_________________________");
+        System.out.println("Nome: " + nome1);
+        System.out.println("Data de Nascimento: " + data1);
+    }
+}
